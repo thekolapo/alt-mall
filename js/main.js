f0780103da0705ee1a5a4cb6f6221d4b134bf0b7
Kolapo Oni <onikolapo@gmail.com>
2018-07-10 09:50:56 +0100

    fixed issues with cart, image switching in product section, div replacemnets with buttons, image not displaying

--- js/main.js
+++ js/main.js
@@ -27,5 +27,50 @@ function gotoProductListPage(){
 }
 
 function showCart(){
-    document.getElementById("cart-slide").style.width = "320px";
+    document.getElementById("cart-slide").style.width = "330px";
+}
+
+function switchBetweenProductsAndVendors(index){
+    for(var i = 0; i<2; i++){
+        document.getElementsByClassName("nav-button").item(i).classList.add("background-none");
+    }
+    document.getElementsByClassName("nav-button").item(index).classList.remove("background-none");
+
+    if(index == 0){
+        document.getElementsByClassName("nav-bottom-products").item(0).style.display = "block";
+        document.getElementsByClassName("nav-bottom-vendors").item(0).style.display = "none";
+    }
+    else{
+        document.getElementsByClassName("nav-bottom-products").item(0).style.display = "none";
+        document.getElementsByClassName("nav-bottom-vendors").item(0).style.display = "block";
+    }
+}
+
+var currentProductImageIndex = 0;
+
+function switchProductImage(index){
+    if(index == "prev"){
+       if(currentProductImageIndex == 0){
+           currentProductImageIndex = 3;
+       }
+       else{
+           currentProductImageIndex -= 1;
+       }
+
+       index = currentProductImageIndex;
+    }
+    else if(index == "next"){
+        if(currentProductImageIndex == 3){
+            currentProductImageIndex = 0;
+        }
+        else{
+            currentProductImageIndex += 1;
+        }
+
+        index = currentProductImageIndex;
+    }
+    
+    var imgSrc = document.getElementById("product-small-img-"+index).src;
+    document.getElementById("product-big-image").src = imgSrc;
+    currentProductImageIndex = index;
 }
